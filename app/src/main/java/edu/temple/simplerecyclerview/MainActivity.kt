fd8125ac5a4872886a6f002ea29ad35b61b43403
goosayhimmet <goosay.himmet@gmail.com>
2025-02-13 12:12:36 -0500

    Defined and passed lambda to adapter

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -6,23 +6,25 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        // Set up RecyclerView display responsibility
-        val numbers = Array(100) {it + 1}
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        recyclerView.layoutManager = GridLayoutManager(this, 4)
+            // Set up RecyclerView display responsibility
+            val numbers = Array(100) {it + 1}
+            val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+            val displayTextView = findViewById<TextView>(R.id.displayTextView)
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
 
-        // TODO (Step 2: Define lambda to modify displayTextView size)
+            // TODO (Step 2: Define lambda to modify displayTextView size)
 
-        // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+            // Todo (Step 3: Pass lambda to adapter)
+            recyclerView.adapter = NumberDisplayAdapter(numbers){size : Int ->
+                displayTextView.textSize = size.toFloat()
+            }
 
+
+        }
 
     }
-
-}
